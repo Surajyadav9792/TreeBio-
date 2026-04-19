e098e0724c5d4ad48497e72822e9b8aae2ff6173
Suraj Yadav <surajyadav2823510@gmail.com>
2026-04-19 16:36:43 +0530

    Fix client-server issue and add username suggestion feature

---
+++ module/profile/actions/index.tsx
@@ -0,0 +1,43 @@
+"use server";
+
+import {db} from "@/lib/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { getAvailableUsernameSuggestons } from "../utils"
+export const checkProfileUsernameAvailability =async (username:string)=>{
+    if(!username) return {available:false,suggestions:[]}
+
+    const user =await db.user.findUnique({
+        where:{
+            username:username
+           } 
+         })
+        if(!user){
+            return {available:true};  
+        }
+   const suggestions=await getAvailableUsernameSuggestons(username,3,10)
+
+   return {
+    available:false,
+    suggestions
+   }
+}
+
+export const claimUsername=async (username:string) => {
+    const loggedUser=await currentUser();
+
+    if(!loggedUser) return {
+        success:false,error:"No authenticated user found"
+    }
+    const user = await db.user.update({
+        where:{
+          clerkId:loggedUser.id   
+        },
+        data:{
+           username:username
+        }
+    })
+    if(!user) return {
+        success:false,error:"No authenticated user found"
+    }
+    return{sucess:true};
+}
